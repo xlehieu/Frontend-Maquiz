212beba1910741f33754ac77d3ea0b0718bd5764
xlehieu <xlehieu@gmail.com>
2025-04-08 17:14:05 +0700

    kkkk

--- src/services/post.service.js
+++ src/services/post.service.js
@@ -3,12 +3,11 @@ export const createPost = async (data) => {
     try {
         const { content } = data;
         if (!content) {
-            throw 'Phải có nội dung thông báo cho lớp học';
+            throw new Error('Lỗi');
         }
         const res = await axiosCredentials.post('/post', JSON.stringify(data));
         if (res.status === 200) return 'Đăng bài thành công';
     } catch (err) {
-        console.log(err);
         if (err.response) throw new Error(err.response.data.message || 'Thông báo thất bại');
     }
 };
@@ -24,7 +23,7 @@ export const getPostsByClassroomId = async (data) => {
 export const deletePostById = async (data) => {
     try {
         const { postIdToDelete } = data;
-        if (!postIdToDelete) throw 'Lỗi';
+        if (!postIdToDelete) throw new Error('Lỗi');
         const res = await axiosCredentials.delete(`/post/${postIdToDelete}`);
         if (res.status === 200) return 'Xóa bài thành công';
     } catch (err) {}
--- src/services/quizHistory.service.js
+++ src/services/quizHistory.service.js
@@ -3,7 +3,7 @@ import axiosCredentials from '../config/axios.credential';
 export const saveQuizHistory = async (data) => {
     try {
         const { score, quizId, answerChoices } = data;
-        if (!score || !quizId || !answerChoices) throw 'Lỗi';
+        if (!score || !quizId || !answerChoices) throw new Error('Lỗi');
         const res = await axiosCredentials.post('/quiz-history', JSON.stringify(data));
         if (res.status === 200) return res?.data?.data;
     } catch (err) {
@@ -14,7 +14,7 @@ export const saveQuizHistory = async (data) => {
 export const getMyExamHistory = async (data) => {
     try {
         const { score, quizId, answerChoices } = data;
-        if (!score || !quizId || !answerChoices) throw 'Lỗi';
+        if (!score || !quizId || !answerChoices) throw new Error('Lỗi');
         const res = await axiosCredentials.get('/quiz-history/mine');
         if (res.status === 200) return res?.data?.message;
     } catch (err) {
